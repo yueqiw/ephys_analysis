747b1a6eb5fe0010d0011997f9b239d922360fb8
Yueqi Wang <yueqi.wang.pku@gmail.com>
2018-05-29 19:56:05 -0600

    optimize adaptation calculation. add new features to feature dict.

--- current_clamp_features.py
+++ current_clamp_features.py
@@ -6,11 +6,26 @@ from allensdk_0_14_2 import ephys_extractor as efex
 from allensdk_0_14_2 import ephys_features as ft
 
 
+feature_name_dict = dict(input_resistance='Input Resistance (MOhm)',
+                     sag='Sag',
+                     capacitance='Membrane Capacitance (pF)',
+                     v_rest='Resting Vm (mV)',
+                    f_i_curve_slope='F-I Curve Slope',
+                    ap_threshold='Spike threshold (mV)',
+                    ap_width='Spike Width (ms)',
+                    ap_peak_to_threshold='Spike Amplitude (mV)',
+                    ap_trough_to_threshold='AHP Amplitude (mV)',
+                    ap_upstroke='Spike Upstroke (mV/ms)',
+                    ap_updownstroke_ratio='Upstroke-Downstroke Ratio',
+                    adapt_avg='Adaptation Index',
+                    vm_for_sag='Vm for Sag (mV)')
+
 def extract_istep_features(data, start, end, subthresh_min_amp = -100, n_subthres_sweeps = 4,
                             sag_target = -100, hero_delta_mV = 10,
                             filter=10., dv_cutoff=5., max_interval=0.02, min_height=5.,
                             min_peak=-20., thresh_frac=0.05, baseline_interval=0.1,
-                            baseline_detect_thresh = 0.3):
+                            baseline_detect_thresh = 0.3, spike_detection_delay=0.001,
+                            adapt_avg_n_sweeps=3, adapt_first_n_ratios=2):
 
     '''
     Compute the cellular ephys features from square pulse current injections.
@@ -34,6 +49,7 @@ def extract_istep_features(data, start, end, subthresh_min_amp = -100, n_subthre
                                 min_peak=min_peak, thresh_frac=thresh_frac,
                                 baseline_interval=baseline_interval,
                                 baseline_detect_thresh=baseline_detect_thresh,
+                                spike_detection_delay=spike_detection_delay,
                                 id_set=list(range(data['n_sweeps'])))
 
     # only extract long_sqaures features
@@ -83,12 +99,7 @@ def extract_istep_features(data, start, end, subthresh_min_amp = -100, n_subthre
     else:
         print("Could not find hero sweep.")
 
-    v_baseline = np.mean(fex.long_squares_features().sweep_features('v_baseline'))
-    bias_current = np.mean(fex.long_squares_features().sweep_features('i_baseline'))
-
     first_spike = cell_features['rheobase_sweep']['spikes'][0] if has_AP else {}
-    cell_features['v_baseline'] = v_baseline
-    cell_features['bias_current'] = bias_current
     cell_features['hero_sweep'] = hero_sweep.as_dict() if hero_sweep else {}
     cell_features['hero_sweep_stim_amp'] = cell_features['hero_sweep']['stim_amp'] if hero_sweep else None
     cell_features['hero_sweep_index'] = cell_features['hero_sweep']['id'] if hero_sweep else None
@@ -97,23 +108,20 @@ def extract_istep_features(data, start, end, subthresh_min_amp = -100, n_subthre
 
     spikes_sweep_id = np.array([swp['id'] for swp in cell_features['spiking_sweeps'] for spike in swp['spikes']])
     all_spikes = [spike for swp in cell_features['spiking_sweeps'] for spike in swp['spikes']]
-    
-    spikes_threshold_t = np.array([spike['threshold_t'] for spike in all_spikes])
+
     spikes_peak_t = np.array([spike['peak_t'] for spike in all_spikes])
-    spikes_trough_t = np.array([spike['trough_t'] for spike in all_spikes])
-    spikes_threshold_v = np.array([spike['threshold_v'] for spike in all_spikes])
-    spikes_peak_v = np.array([spike['peak_v'] for spike in all_spikes])
-    spikes_trough_v = np.array([spike['trough_v'] for spike in all_spikes])
-
-    adapt_avg = calculate_adapt(spikes_sweep_id, spikes_peak_t, start, end,
-                                    adapt_interval=1.0, max_isi_ratio=2.5)
-
+    adapt_avg, adapt_all = calculate_adapt(spikes_sweep_id, spikes_peak_t, start,
+                                    adapt_interval=1.0, max_isi_ratio=3,
+                                    avg_n_sweeps=adapt_avg_n_sweeps,
+                                    first_n_adapt_ratios=adapt_first_n_ratios)
+    # print(adapt_avg)
+    # print(adapt_all)
 
     summary_features = OrderedDict([
                         ('file_id', data['file_id']),
                         ('has_ap', has_AP),
-                        ('v_baseline', v_baseline),
-                        ('bias_current', bias_current),
+                        ('v_baseline', cell_features['v_baseline']),
+                        ('bias_current', cell_features['bias_current']),
                         ('tau', cell_features['tau'] * 1000),
                         ('capacitance' , cell_features['tau'] / cell_features['input_resistance'] * 10**6 \
                                         if cell_features['input_resistance'] > 0 else None),
@@ -124,6 +132,8 @@ def extract_istep_features(data, start, end, subthresh_min_amp = -100, n_subthre
 
                         ('sag', cell_features['sag']),
                         ('vm_for_sag', cell_features['vm_for_sag']),
+                        ('indices_for_sag', cell_features["indices_for_sag"]),
+                        ('sag_sweep_indices', cell_features["sag_sweeps"]),
 
                         ('ap_threshold', first_spike.get('threshold_v')),
                         ('ap_width', first_spike.get('width') * 1000 if not first_spike.get('width') is None else None),
@@ -149,18 +159,20 @@ def extract_istep_features(data, start, end, subthresh_min_amp = -100, n_subthre
 
                         ('all_firing_rate', np.array([swp['avg_rate'] for swp in cell_features['sweeps']])),
                         ('all_stim_amp', np.array([swp['stim_amp'] for swp in cell_features['sweeps']])),
+                        ('input_resistance_vm', cell_features['input_resistance_vm']),
+                        ('input_resistance_stim_ap', cell_features['input_resistance_stim_ap']),
                         ('all_adaptation', np.array([swp.get('adapt', np.nan) for swp in cell_features['sweeps']])),
                         ('all_v_baseline', np.array([swp['v_baseline'] for swp in cell_features['sweeps']])),
                         ('all_median_isi', np.array([swp.get('median_isi', np.nan) for swp in cell_features['sweeps']])),
                         ('all_first_isi', np.array([swp.get('first_isi', np.nan) for swp in cell_features['sweeps']])),
                         ('all_latency', np.array([swp.get('latency', np.nan) for swp in cell_features['sweeps']])),
                         ('spikes_sweep_id', spikes_sweep_id),
-                        ('spikes_threshold_t', spikes_threshold_t),
+                        ('spikes_threshold_t', np.array([spike['threshold_t'] for spike in all_spikes])),
                         ('spikes_peak_t', spikes_peak_t),
-                        ('spikes_trough_t', spikes_trough_t),
-                        ('spikes_threshold_v', spikes_threshold_v),
-                        ('spikes_peak_v', spikes_peak_v),
-                        ('spikes_trough_v', spikes_trough_v),
+                        ('spikes_trough_t', np.array([spike['trough_t'] for spike in all_spikes])),
+                        ('spikes_threshold_v', np.array([spike['threshold_v'] for spike in all_spikes])),
+                        ('spikes_peak_v', np.array([spike['peak_v'] for spike in all_spikes])),
+                        ('spikes_trough_v', np.array([spike['trough_v'] for spike in all_spikes])),
                         ('adapt_avg', adapt_avg)
 
     ])
@@ -168,10 +180,21 @@ def extract_istep_features(data, start, end, subthresh_min_amp = -100, n_subthre
     return cell_features, summary_features
 
 
-def calculate_adapt(spikes_sweep_id, spikes_peak_t, start, end, adapt_interval=1.0,
-                    min_peaks=3, max_isi_ratio=2.5, firing_rate_target=None):
+def calculate_adapt(spikes_sweep_id, spikes_peak_t, start, end=None, adapt_interval=1.0,
+                    min_peaks=3, max_isi_ratio=3, avg_n_sweeps=3, first_n_adapt_ratios=None,
+                    firing_rate_target=None):
+    '''
+    parameters
+    ----------
+    max_isi_ratio: filter out long gaps .
+    avg_n_sweeps: use the first n sweeps that satisfy all constraints to calculate average adaptation ratio.
+    first_n_adapt_ratios: for each sweep, only take the first n adaptation ratios for averaging.
+                Setting this to None then uses all adaptation ratios from the sweep.
+                setting this to 1 or 2 adjust for the fact that many neurons only fire a few (3-4) spikes,
+                    allowing comparison between high firing rate neurons with low firing rate neurons.
+    '''
     if len(spikes_sweep_id) == 0:
-        return None
+        return None, None
     end_adapt = start + adapt_interval
     sweep_id = spikes_sweep_id[spikes_peak_t < end_adapt]
     peaks_all = spikes_peak_t[spikes_peak_t < end_adapt]
@@ -191,7 +214,7 @@ def calculate_adapt(spikes_sweep_id, spikes_peak_t, start, end, adapt_interval=1
     for k in to_pop:
         peaks.pop(k)
     if len(peaks) == 0:
-        return None
+        return None, None
 
     # calculate isi
     isi = dict()
@@ -213,11 +236,12 @@ def calculate_adapt(spikes_sweep_id, spikes_peak_t, start, end, adapt_interval=1
     for k in to_pop:
         isi.pop(k)
     if len(isi) == 0:
-        return None
+        return None, None
 
     # only take the first 3 sweeps with isi data
-    if len(isi) > 3:
-        keys = sorted(list(isi.keys()))[:3]
+    # assuming sweep id increments with higher current injection
+    if len(isi) > avg_n_sweeps:
+        keys = sorted(list(isi.keys()))[:avg_n_sweeps]
         isi = {k: isi[k] for k in isi if k in keys}
 
     # calculate adaptation
@@ -227,6 +251,7 @@ def calculate_adapt(spikes_sweep_id, spikes_peak_t, start, end, adapt_interval=1
 
     #print(adapt)
     # take median adaptation from each sweep, then average the 3 sweeps
-    adapt_mean = np.mean([np.median(adapt[k]) for k in adapt])
+    adapt_all = [np.mean(adapt[k][:first_n_adapt_ratios]) for k in adapt]
+    adapt_mean = np.mean(adapt_all)
 
-    return adapt_mean
+    return adapt_mean, adapt_all

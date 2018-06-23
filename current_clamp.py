4af73e93225dc8cc837aa5a226df37f4eb3160a2
Yueqi Wang <yueqi.wang.pku@gmail.com>
2018-06-22 21:34:53 -0600

    add rasterize option, change lw and colors for highlighting sweeps. add trough markers. add dvdt and d2vdt2 plots.

--- current_clamp.py
+++ current_clamp.py
@@ -61,14 +61,17 @@ def load_current_step_add_itrace(abf_file, ihold, istart, istep, startend=None, 
 
 
 def plot_current_step(data, fig_height=6, x_scale=3.5, xlim=[0.3,3.2],
-                        startend=None, offset=[0.2, 0.4],
+                        startend=None, offset=[0.2, 0.4], lw_scale=1, alpha_scale=1,
+                        plot_gray_sweeps=True,
                         blue_sweep=None, rheobase_sweep=None, sag_sweeps=[], vlim=[-145,60], ilim=[-95,150],
-                        spikes_sweep_id = None, spikes_t = None, other_features=None,
+                        spikes_sweep_id = None, spikes_t = None,
+                        other_features=None, trough_name = 'spikes_trough_5w',
                         bias_current = 0.0,
                         highlight = 'deepskyblue',
                         highlight_rheobase=sns.color_palette("muted").as_hex()[2],
                         highlight_sag=sns.color_palette("muted").as_hex()[4],
-                        skip_sweep=1, skip_point=10, save=False):
+                        skip_sweep=1, skip_point=10, save=False,
+                        rasterized=True):
     '''
     Plot overlayed sweeps in current clamp protocol, with one sweep in blue color
     If detected spikes are provided, also plot detected spikes.
@@ -96,7 +99,7 @@ def plot_current_step(data, fig_height=6, x_scale=3.5, xlim=[0.3,3.2],
         figsize = (fig_width, fig_height)
 
     fig = plt.figure(figsize=figsize)
-    fig.patch.set_alpha(0.1)
+    fig.patch.set_alpha(0)
     gs = gridspec.GridSpec(n_plots, 1, height_ratios=height_ratios)
 
     axes = [plt.subplot(gs[x]) for x in range(n_plots)]
@@ -119,59 +122,68 @@ def plot_current_step(data, fig_height=6, x_scale=3.5, xlim=[0.3,3.2],
     for i in indices[::-1]:
         if i == rheobase_sweep:
             color = highlight_rheobase
-            lw=1.25
-            size=8
-            alpha=1
+            lw=1.25 * lw_scale
+            size=8 * lw_scale
+            alpha=1 * alpha_scale
         elif i == blue_sweep or i == data['n_sweeps'] + blue_sweep:
             color = highlight
-            lw=1.25
-            size=8
-            alpha=1
+            lw=1.25 * lw_scale
+            size=8 * lw_scale
+            alpha=1 * alpha_scale
         elif i in sag_sweeps:
             color = highlight_sag
-            lw=0.5
-            size=3
-            alpha=1
+            lw=1 * lw_scale
+            size=8 * lw_scale
+            alpha=1 * alpha_scale
         else:
             color = 'gray'
-            lw=0.2
-            size=3
-            alpha=0.6
+            lw=0.2 * lw_scale
+            size=3 * lw_scale
+            alpha=0.6 * alpha_scale
+            if not plot_gray_sweeps:
+                continue
 
-        axes[-2].plot(data['t'][::skip_point], data['voltage'][i][::skip_point], color=color, lw=lw, alpha=alpha)
+        axes[-2].plot(data['t'][::skip_point], data['voltage'][i][::skip_point],
+                        color=color, lw=lw, alpha=alpha, rasterized=rasterized)
         if i == rheobase_sweep and not other_features is None:
             threshold_t = other_features['spikes_threshold_t'][spikes_sweep_id==i]
             threshold_v = other_features['spikes_threshold_v'][spikes_sweep_id==i]
-            trough_t = other_features['spikes_trough_t'][spikes_sweep_id==i]
-            trough_v = other_features['spikes_trough_v'][spikes_sweep_id==i]
+            trough_t = other_features[trough_name + '_t'][spikes_sweep_id==i]
+            trough_v = other_features[trough_name + '_v'][spikes_sweep_id==i]
             axes[-2].scatter(threshold_t, threshold_v, marker='_', s=30, lw=1, c="black", alpha=alpha)
             axes[-2].scatter(trough_t, trough_v, marker='+', s=30, lw=1, c="black", alpha=alpha)
-        axes[-2].set_ylim(vlim)
-        axes[-2].set_ylabel('Membrane Voltage (mV)', fontsize=16)
-        axes[-2].set_xticklabels([])
 
-        axes[-1].plot(data['t'][::skip_point], data['current'][i][::skip_point] - bias_current, color=color, lw=lw, alpha=alpha)
-        axes[-1].set_ylim(ilim)
-        axes[-1].set_ylabel('Current (pA)', fontsize=16)
-        axes[-1].set_xlabel('Time (s)', fontsize=16)
+        axes[-1].plot(data['t'][::skip_point], data['current'][i][::skip_point] - bias_current,
+                        color=color, lw=lw, alpha=alpha, rasterized=rasterized)
 
         if n_plots == 3:
             spikes = spikes_t[spikes_sweep_id==i]
             axes[0].scatter(spikes, np.ones_like(spikes) * i, marker='o', s=size, c=color, alpha=alpha)
-            axes[0].set_ylim([1, data['n_sweeps']])
-            axes[0].set_xticklabels([])
-            axes[0].set_ylabel('Sweeps', fontsize=16)
+
+
+    axes[-2].set_ylim(vlim)
+    axes[-2].set_ylabel('Membrane Voltage (mV)', fontsize=16)
+    axes[-2].set_xticklabels([])
+    axes[-1].set_ylim(ilim)
+    axes[-1].set_ylabel('Current (pA)', fontsize=16)
+    axes[-1].set_xlabel('Time (s)', fontsize=16)
+    axes[0].set_ylim([1, data['n_sweeps']])
+    axes[0].set_xticklabels([])
+    axes[0].set_ylabel('Sweeps', fontsize=16)
 
     for ax in axes:
         ax.set_xlim(xlim)
         ax.yaxis.set_label_coords(-0.64/figsize[0],0.5)
-        ax.patch.set_alpha(0.2)
+        ax.patch.set_alpha(0)
+        ax.grid(False)
+        for loc in ['top', 'right', 'bottom', 'left']:
+            ax.spines[loc].set_visible(False)
 
     plt.tight_layout()
     if save is True:
         plt.savefig(os.path.join(data['file_directory'], data['file_id']) + '.png', dpi=300)
         #plt.savefig(os.path.join(data['file_directory'], data['file_id']) + '.svg')
-        plt.savefig(os.path.join(data['file_directory'], data['file_id']) + '.pdf')
+        plt.savefig(os.path.join(data['file_directory'], data['file_id']) + '.pdf', dpi=300)
 
     return fig
 
@@ -238,7 +250,7 @@ def animate_current_step(data, fig_height=6, x_scale=3.5, xlim=[0.3,3.2],
     for ax in axes:
         ax.set_xlim(xlim)
         ax.yaxis.set_label_coords(-0.64/figsize[0],0.5)
-        ax.patch.set_alpha(0.2)
+        ax.patch.set_alpha(0)
 
     plt.tight_layout()
 
@@ -277,45 +289,49 @@ def animate_current_step(data, fig_height=6, x_scale=3.5, xlim=[0.3,3.2],
     return fig, anim
 
 
-def plot_fi_curve(stim_amp, firing_rate, save_filepath = None, color=sns.color_palette("muted").as_hex()[0]):
+def plot_fi_curve(stim_amp, firing_rate, figsize=(4,4), save_filepath = None, color=sns.color_palette("muted").as_hex()[0]):
     '''
     Plot F-I curve
     '''
     mpl.rcParams.update(mpl.rcParamsDefault)
-    fig, ax = plt.subplots(1,1,figsize=(4,4))
+    fig, ax = plt.subplots(1,1,figsize=figsize)
     ax.plot(stim_amp, firing_rate, marker='o', linewidth=1.5, markersize=8, color=color)
     fig.gca().spines['right'].set_visible(False)
     fig.gca().spines['top'].set_visible(False)
     ax.set_ylabel('Spikes per second', fontsize=18)
     ax.set_xlabel('Current (pA)', fontsize=18)
-    ax.yaxis.set_label_coords(-0.22,0.5)
+    ax.yaxis.set_label_coords(-0.22 * 4 / figsize[0],0.5)
     ax.tick_params(axis = 'both', which = 'major', labelsize = 14)
+    ax.patch.set_alpha(0)
     fig.tight_layout()
     if save_filepath is not None:
         fig.savefig(save_filepath, dpi=200)
     return fig
 
-def plot_vi_curve(stim_amp, voltage, save_filepath = None, color="gray"):
+def plot_vi_curve(stim_amp, voltage, figsize=(4,4), save_filepath = None, color="gray"):
     '''
     Plot V-I curve
     '''
     mpl.rcParams.update(mpl.rcParamsDefault)
-    fig, ax = plt.subplots(1,1,figsize=(4,4))
+    fig, ax = plt.subplots(1,1,figsize=figsize)
     ax.plot(stim_amp, voltage, marker='o', linewidth=1.5, markersize=8, color=color)
     fig.gca().spines['right'].set_visible(False)
     fig.gca().spines['top'].set_visible(False)
     ax.set_ylabel('Voltage (mV)', fontsize=18)
     ax.set_xlabel('Current (pA)', fontsize=18)
-    ax.yaxis.set_label_coords(-0.22,0.5)
+    ax.yaxis.set_label_coords(-0.22 * 4 / figsize[0],0.5)
     ax.tick_params(axis = 'both', which = 'major', labelsize = 14)
+    ax.patch.set_alpha(0)
     fig.tight_layout()
     if save_filepath is not None:
         fig.savefig(save_filepath, dpi=200)
     return fig
 
-def plot_first_spike(data, features, time_zero='threshold',
+
+def plot_first_spike(data, features, time_zero='threshold', figsize=(4,4), lw_scale=1,
                     window=None, vlim=[-80, 60], color=sns.color_palette("muted").as_hex()[2],
-                    save_filepath = None):
+                    other_markers = dict(),
+                    save_filepath = None, rasterized=False):
     '''
     Plot the first action potential. Time window is something like:
     Inputs
@@ -345,31 +361,42 @@ def plot_first_spike(data, features, time_zero='threshold',
     v = data['voltage'][features['rheobase_index']][start:end]
 
     mpl.rcParams.update(mpl.rcParamsDefault)
-    fig, ax = plt.subplots(1,1,figsize=(4,4))
-    ax.plot(t, v, color=color, lw=2)
+    fig, ax = plt.subplots(1,1,figsize=figsize)
+    ax.plot(t, v, color=color, lw=2 * lw_scale, rasterized=rasterized)
 
     threshold_time = (features['spikes_threshold_t'][0] - t0) * 1000
     ax.hlines(features['ap_threshold'], window[0], threshold_time,
                 linestyles='dotted', color='grey')
+
+    for i, (feature, col) in enumerate(other_markers.items()):
+        feature_t = (features[feature + '_t'][0] - t0) * 1000
+        feature_v = features[feature + '_v'][0]
+        if feature_t > window[1]:
+            continue
+        ax.scatter(feature_t, vlim[0] + i*2 + 2, marker='+', s=50, lw=1.5, c=col)
+
 
     ax.set_ylim(vlim)
     fig.gca().spines['right'].set_visible(False)
     fig.gca().spines['top'].set_visible(False)
     ax.set_ylabel('Voltage (mV)', fontsize=18)
     ax.set_xlabel('Time (ms)', fontsize=18)
-    ax.yaxis.set_label_coords(-0.22,0.5)
+    ax.yaxis.set_label_coords(-0.22 * 4 / figsize[0],0.5)
     ax.tick_params(axis = 'both', which = 'major', labelsize = 14)
+    ax.patch.set_alpha(0)
+    ax.grid(False)
+    for loc in ['top', 'right', 'bottom', 'left']:
+        ax.spines[loc].set_visible(False)
     fig.tight_layout()
 
     if save_filepath is not None:
         fig.savefig(save_filepath, dpi=200)
     return fig
 
-
-def plot_phase_plane(data, features, filter=None, window=[-50, 200],
+def plot_phase_plane(data, features, filter=None, figsize=(4, 4), window=[-50, 200], lw_scale=1,
                         vlim=[-80, 60], dvdtlim=[-80, 320],
                         color=sns.color_palette("muted").as_hex()[1],
-                        save_filepath=None):
+                        save_filepath=None, rasterized=False):
     t0 = features['spikes_threshold_t'][0]
     ap_window = [t0 + x * 0.001 for x in window]
 
@@ -380,11 +407,12 @@ def plot_phase_plane(data, features, filter=None, window=[-50, 200],
     start, end = [ft.find_time_index(data['t'], x) for x in ap_window]
     t = (data['t'][start:end] - data['t'][start]) * 1000 + window[0]
     v = data['voltage'][features['rheobase_index']][start:end]
-    dvdt = ft.calculate_dvdt(v, t, filter=filter) * 1000
+    # dvdt = ft.calculate_dvdt(v, t, filter=filter) * 1000
+    dvdt = ft.calculate_dvdt(v, data['t'][start:end], filter)  # filter=10 or 5
 
     mpl.rcParams.update(mpl.rcParamsDefault)
-    fig, ax = plt.subplots(1,1,figsize=(4, 4))
-    ax.plot(v[0:-1], dvdt, color=color, lw=2)
+    fig, ax = plt.subplots(1,1,figsize=figsize)
+    ax.plot(v[0:-1], dvdt, color=color, lw=2 * lw_scale, rasterized=rasterized)
 
     ax.set_xlim(vlim)
     ax.set_ylim(dvdtlim)
@@ -392,14 +420,146 @@ def plot_phase_plane(data, features, filter=None, window=[-50, 200],
     fig.gca().spines['top'].set_visible(False)
     ax.set_xlabel('Voltage (mV)', fontsize=18)
     ax.set_ylabel('dV/dt (V/s)', fontsize=18)
-    ax.yaxis.set_label_coords(-0.22,0.5)
+    ax.yaxis.set_label_coords(-0.22 * 4 / figsize[0],0.5)
     ax.tick_params(axis = 'both', which = 'major', labelsize = 14)
+    ax.patch.set_alpha(0)
+    ax.grid(False)
+    for loc in ['top', 'right', 'bottom', 'left']:
+        ax.spines[loc].set_visible(False)
     fig.tight_layout()
 
     if save_filepath is not None:
         fig.savefig(save_filepath, dpi=200)
     return fig
 
+
+def plot_first_spike_dvdt(data, features, time_zero='threshold', figsize=(4,4),
+                    filter_dvdt=10,
+                    window=None, ylim=None,
+                    color="gray",
+                    save_filepath = None, rasterized=False):
+    '''
+    Plot dv/dt of the first action potential. Time window is something like:
+    Inputs
+    -----
+    data: raw data of sweeps loaded by load_current_step()
+    features: dictionary from extract_istep_features()
+    time_zero: whether to use threshold or peak time
+    window: time range in ms. such as [t-10, t+40] ms
+
+    Returns
+    -------
+    figure object
+    '''
+    assert(time_zero in ['threshold', 'peak'])
+    if time_zero == 'threshold':
+        t0 = features['spikes_threshold_t'][0]
+        if window is None:
+            window = [-10, 40]
+    elif time_zero == 'peak':
+        t0 = features['spikes_peak_t'][0]
+        if window is None:
+            window = [-15, 35]
+
+    ap_window = [t0 + x * 0.001 for x in window]
+    start, end = [ft.find_time_index(data['t'], x) for x in ap_window]
+    t = (data['t'][start:end] - data['t'][start]) * 1000 + window[0]
+    v = data['voltage'][features['rheobase_index']][start:end]
+
+    #dvdt = ft.calculate_dvdt(v, t, filter=filter_dvdt) * 1000  need filter=0.01 or 0.005
+    dvdt = ft.calculate_dvdt(v, data['t'][start:end], filter_dvdt)  # filter=10 or 5
+
+    mpl.rcParams.update(mpl.rcParamsDefault)
+    fig, ax = plt.subplots(1,1,figsize=figsize)
+    ax.plot(t[:-1], dvdt, color=color, lw=2, rasterized=rasterized)
+
+    threshold_time = (features['spikes_threshold_t'][0] - t0) * 1000
+    dvdt_thres_index = ft.find_time_index(data['t'], features['spikes_threshold_t'][0]) - start
+    ax.hlines(dvdt[dvdt_thres_index], window[0], threshold_time,
+                linestyles='dotted', color='grey')
+
+    ax.set_ylim(ylim)
+    fig.gca().spines['right'].set_visible(False)
+    fig.gca().spines['top'].set_visible(False)
+    ax.set_ylabel('dv/dt (mV/ms)', fontsize=18)
+    ax.set_xlabel('Time (ms)', fontsize=18)
+    ax.yaxis.set_label_coords(-0.22 * 4 / figsize[0],0.5)
+    ax.tick_params(axis = 'both', which = 'major', labelsize = 14)
+    ax.patch.set_alpha(0)
+    ax.grid(False)
+    for loc in ['top', 'right', 'bottom', 'left']:
+        ax.spines[loc].set_visible(False)
+    fig.tight_layout()
+
+    if save_filepath is not None:
+        fig.savefig(save_filepath, dpi=200)
+    return fig
+
+
+
+def plot_first_spike_2nd_derivative(data, features, time_zero='threshold', figsize=(4,4),
+                    filter_dvdt=10,
+                    window=None, ylim=None,
+                    color="gray",
+                    save_filepath = None, rasterized=False):
+    '''
+    Plot dv/dt of the first action potential. Time window is something like:
+    Inputs
+    -----
+    data: raw data of sweeps loaded by load_current_step()
+    features: dictionary from extract_istep_features()
+    time_zero: whether to use threshold or peak time
+    window: time range in ms. such as [t-10, t+40] ms
+
+    Returns
+    -------
+    figure object
+    '''
+    assert(time_zero in ['threshold', 'peak'])
+    if time_zero == 'threshold':
+        t0 = features['spikes_threshold_t'][0]
+        if window is None:
+            window = [-10, 40]
+    elif time_zero == 'peak':
+        t0 = features['spikes_peak_t'][0]
+        if window is None:
+            window = [-15, 35]
+
+    ap_window = [t0 + x * 0.001 for x in window]
+    start, end = [ft.find_time_index(data['t'], x) for x in ap_window]
+    t = (data['t'][start:end] - data['t'][start]) * 1000 + window[0]
+    v = data['voltage'][features['rheobase_index']][start:end]
+
+    #dvdt = ft.calculate_dvdt(v, t, filter=filter_dvdt) * 1000
+    #d2vdt2 = ft.calculate_dvdt(dvdt, t[:-1], filter=filter_dvdt) * 1000
+    dvdt = ft.calculate_dvdt(v, data['t'][start:end], filter_dvdt)
+    d2vdt2 = ft.calculate_dvdt(dvdt, data['t'][start:end-1], filter_dvdt)
+
+    mpl.rcParams.update(mpl.rcParamsDefault)
+    fig, ax = plt.subplots(1,1,figsize=figsize)
+    ax.plot(t[:-2], d2vdt2, color=color, lw=1.5, rasterized=rasterized)
+
+    threshold_time = (features['spikes_threshold_t'][0] - t0) * 1000
+    dvdt_thres_index = ft.find_time_index(data['t'], features['spikes_threshold_t'][0]) - start
+    ax.hlines(d2vdt2[dvdt_thres_index], window[0], threshold_time,
+                linestyles='dotted', color='grey')
+
+    ax.set_ylim(ylim)
+    fig.gca().spines['right'].set_visible(False)
+    fig.gca().spines['top'].set_visible(False)
+    ax.set_ylabel('d2v/dt2 (mV/ms^2)', fontsize=18)
+    ax.set_xlabel('Time (ms)', fontsize=18)
+    ax.yaxis.set_label_coords(-0.22 * 4 / figsize[0],0.5)
+    ax.tick_params(axis = 'both', which = 'major', labelsize = 14)
+    ax.patch.set_alpha(0)
+    ax.grid(False)
+    for loc in ['top', 'right', 'bottom', 'left']:
+        ax.spines[loc].set_visible(False)
+    fig.tight_layout()
+
+    if save_filepath is not None:
+        fig.savefig(save_filepath, dpi=200)
+    return fig
 
 def combine_vertical(images, scale = 1):
     # combine multiple PIL images
